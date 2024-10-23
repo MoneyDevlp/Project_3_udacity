a69487e97fba57c2c2ba0f7175bb5bffa5470995
MoneyDevlp <leo1172001@gmail.com>
2024-10-23 23:24:21 +0700

    update src

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=tien
+export POSTGRES_USERNAME=tienlv
 export POSTGRES_PASSWORD=db117001
-export POSTGRES_HOST=dbpj3.cs84egkhjh5y.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-pj3.cs84egkhjh5y.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=udacity-829186168418
 export AWS_REGION=us-east-1
