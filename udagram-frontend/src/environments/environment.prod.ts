ded15275fc8ce32fd1fb6b030bfe3593d540a6cc
MoneyDevlp <leo1172001@gmail.com>
2024-10-26 14:41:49 +0700

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a47d5825da3214722ab9aecc70b72767-1874948039.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
